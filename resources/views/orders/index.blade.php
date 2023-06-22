3cf8e149728f9c067135f2b7821a5aa8d77c260b
Manuel <104014309+DeveloperWeppy@users.noreply.github.com>
2023-06-22 11:22:15 -0500

    resubiendo ajusted de responsive de tablas

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -58,8 +58,8 @@
 @endsection
 
 @section('scripts')
-    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
     <script src="{{ asset('assets/js/codeScanner/minified/html5-qrcode.min.js') }}"></script>
     <script> 
      var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,8 +4,8 @@
     Usuarios
 @endsection
 @section('css')
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.dataTables.min.css">
 @endsection
 
 @section('main-content')
