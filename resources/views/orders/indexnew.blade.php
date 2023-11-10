51b2f01689f595201070f71f904b59ddb9a83eff
Manuel <104014309+DeveloperWeppy@users.noreply.github.com>
2023-11-10 10:54:27 -0500

    comentando ajax

--- resources/views/orders/indexnew.blade.php
+++ resources/views/orders/indexnew.blade.php
@@ -379,22 +379,22 @@
             //document.getElementById('order_id_input').value = scannedOrderId;
             console.log('order_id_input: '+scannedOrderId);
             // Realiza una solicitud Ajax para verificar el ID del pedido
-            $.ajax({
-                url: '/orders/qr-validation/' + scannedOrderId,
-                method: 'GET',
-                success: function (data) {
-                    if (data.valid) {
-                        //window.location.href = '/orders/create/' + orderId;
-                        console.log("respuesta ajax:" +data.valid)
-                        swal("Correcto!", "Pedido Correcto!", "success");
-                    } else {
-                        swal("Error!", "Número de pedido no válido!", "error");
-                    }
-                },
-                error: function () {
-                    swal("Error!", "Error al verificar el número de pedido!", "error");
-                }
-            });
+            // $.ajax({
+            //     url: '/orders/qr-validation/' + scannedOrderId,
+            //     method: 'GET',
+            //     success: function (data) {
+            //         if (data.valid) {
+            //             //window.location.href = '/orders/create/' + orderId;
+            //             console.log("respuesta ajax:" +data.valid)
+            //             swal("Correcto!", "Pedido Correcto!", "success");
+            //         } else {
+            //             swal("Error!", "Número de pedido no válido!", "error");
+            //         }
+            //     },
+            //     error: function () {
+            //         swal("Error!", "Error al verificar el número de pedido!", "error");
+            //     }
+            // });
         }
     </script>
 @endsection
